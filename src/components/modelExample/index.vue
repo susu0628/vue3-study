bf7f81b33d0e15a7e1948c871148c04a44427c7e
jiangsusu <jiangsusu7586@dingtalk.com>
2021-08-25 20:03:32 +0800

    computed实现自定义v-model

---
+++ src/components/modelExample/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <my-input v-model="message" v-model:checked="checked" />
+  {{ message }} - {{ checked }}
+</template>
+<script>
+import MyInput from "./myInput.vue";
+export default {
+  data: () => {
+    return {
+      message: "",
+      checked: false,
+    };
+  },
+  components: {
+    MyInput,
+  },
+};
+</script>
+<style lang="sass" scoped></style>
---
+++ src/components/modelExample/myInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <input v-model="finalValue" />
+    <input type="checkbox" v-model="finalChecked" />{{ finalChecked }}
+  </div>
+</template>
+<script>
+export default {
+  name: "MyInput",
+  data: () => {
+    return {};
+  },
+  computed: {
+    finalValue: {
+      set(value) {
+        this.$emit("update:modelValue", value);
+      },
+      get() {
+        return this.modelValue;
+      },
+    },
+    finalChecked: {
+      set(value) {
+        this.$emit("update:checked", value);
+      },
+      get() {
+        return this.checked;
+      },
+    },
+  },
+  props: ["modelValue", "checked"],
+  emits: ["update:modelValue", "update:checked"],
+};
+</script>
+<style lang="sass" scoped></style>
