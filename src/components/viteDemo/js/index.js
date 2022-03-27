a52f17dc970ad0afa2b3f08d2e7f45b2f2257aaf
jiangsusu <15798007586@163.com>
2022-03-27 15:38:39 +0800

    vite 初识

---
+++ src/components/viteDemo/js/index.js
@@ -0,0 +1,7 @@
+import { sum } from './math.js';
+// import _ from '../node_modules/lodash-es/lodash.default.js';
+import _ from 'lodash-es';
+
+console.log('hello vite');
+console.log(sum(10, 20));
+console.log(_.join(['jiang', 'tlp'], '-'));
