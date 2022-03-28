0b2f33b527e5e07fef6c5e62953df77e79e5a8e3
jiangsusu <15798007586@163.com>
2022-03-28 21:09:38 +0800

    vite支持less、ts

--- src/components/viteDemo/js/index.js
+++ src/components/viteDemo/js/index.js
@@ -1,7 +1,16 @@
 import { sum } from './math.js';
+import { mult } from '../ts/mult.ts';
 // import _ from '../node_modules/lodash-es/lodash.default.js';
 import _ from 'lodash-es';
+import "../css/index.css";
+import "../css/title.less";
 
 console.log('hello vite');
 console.log(sum(10, 20));
 console.log(_.join(['jiang', 'tlp'], '-'));
+console.log(mult(10, 4));
+
+const titleEle = document.createElement('div')
+titleEle.className = "title";
+titleEle.innerHTML = 'hello vite';
+document.body.appendChild(titleEle);
---
+++ src/components/viteDemo/ts/mult.ts
@@ -0,0 +1,6 @@
+const mult = (num1: number, num2: number): number => {
+  return num1 * num2
+}
+export {
+  mult
+}
